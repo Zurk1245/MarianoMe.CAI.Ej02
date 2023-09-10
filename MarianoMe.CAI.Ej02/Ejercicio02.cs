37c3665c262dbf548a83fba6c646a535a3e19620
Zurk1245 <marianito01@live.com>
2023-09-10 13:31:19 -0300

    Add project files.

---
+++ MarianoMe.CAI.Ej02/Ejercicio02.cs
@@ -0,0 +1,10 @@
+namespace MarianoMe.CAI.Ej02
+{
+    public partial class Ejercicio02 : Form
+    {
+        public Ejercicio02()
+        {
+            InitializeComponent();
+        }
+    }
+}
